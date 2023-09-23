3017ae187a32ab4085f9b04c4e3d4aa9ad6b959d
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-09-23 00:23:36 -0300

    feat: adjusts and remove unnecessary imgs

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -163,7 +163,7 @@ const Header = () => {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              About
+              Sobre nós
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -195,7 +195,7 @@ const Header = () => {
                   d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                 />
               </svg>
-              Feature
+              Metodologia
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -227,7 +227,7 @@ const Header = () => {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              Pricing
+              Segmentos
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -259,7 +259,7 @@ const Header = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
-              Testimonial
+              Contatos
             </LinkScroll>
           </ul>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,27 @@
 import Feature from "../components/Feature";
 import Pricing from "../components/Pricing";
+import Osc from "../components/Osc";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
 import SeoHead from "../components/SeoHead";
 import About from "../components/about";
 import Contact from "../components/contact";
-import { benefitTwo } from "../components/data"
+import { benefitTwo } from "../components/data";
 
 export default function Home() {
   return (
     <>
-      <SeoHead title='Instituto' />
+      <SeoHead title="Instituto" />
       <Layout>
         <Hero />
         <div />
+        <Osc />
         <About />
-        <div id="metodologia"/>
+        <div id="metodologia" />
         <Feature />
-        <div id="segmentos"/>
+        <div id="segmentos" />
         <Pricing />
+        <Osc />
         <Contact imgPos="right" data={benefitTwo} />
       </Layout>
     </>
